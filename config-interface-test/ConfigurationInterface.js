2eca2819daa21c0bd7d60942fb629abe5eb7e667
Guilherme-Elias <guilhermemarinho524@gmail.com>
2025-01-17 23:16:27 -0400

    [feat] : implements configuration interface

---
+++ config-interface-test/ConfigurationInterface.js
@@ -0,0 +1,71 @@
+class ConfigurationInterface
+{
+
+    // ==================
+    // | PRIVATE FIELDS | 
+    // ==================
+
+    #filePath = "./config.json";
+
+    // ==================
+    // | PUBLIC METHODS | 
+    // ==================
+
+    /**
+     * retrieves the configuration data  
+     */  
+    getConfigurationData(){
+        this.#loadConfigurations();
+        return this.configurationData;
+    }
+
+    // ===================
+    // | PRIVATE METHODS | 
+    // ===================
+    
+    /**
+     * opens the configuration file and retrieves its data
+     * @param {String} filePath
+     * @returns {String} fileData
+     */
+    #openConfigurationFile(filePath) {
+        const fileSystem = require("fs");
+
+        const fileData = fileSystem.readFileSync(filePath, "binary", (error, fileData) => {
+
+            if (error) {
+                console.error("Error trying to read the file: ", error);
+                return null;
+            }
+
+            return fileData;
+        });
+
+        return fileData;
+    }
+
+    /**
+     * parses the file data into an object
+     * @param {String} fileData 
+     * @returns {JSON} configurationData
+     */
+    #parseFileData(fileData) {
+        if (fileData == null || fileData.trim() === "") {
+            console.error("The file data is empty.");
+        }
+
+        return JSON.parse(fileData);
+    }
+    
+    /**
+     * loads the configuration data
+     * (i.e.) opens and parses the file into an object
+     */
+    #loadConfigurations() {
+        const fileData = this.#openConfigurationFile(this.#filePath);
+        this.configurationData = this.#parseFileData(fileData);
+    }
+
+}
+
+module.exports = new ConfigurationInterface();
---
+++ config-interface-test/main.js
@@ -0,0 +1,7 @@
+/** this file was created for testing purposes */
+
+const configurationInterface = require("./ConfigurationInterface");
+
+console.log(
+    configurationInterface.getConfigurationData()
+);
