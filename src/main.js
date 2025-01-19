6862a7471fd21b95646ec7a6efa3ff0ab4ee85c7
Martin Czygan <martin.czygan@gmail.com>
2025-01-19 04:38:27 +0100

    feat: markdown preview
    currently, all functionality is packed into the HTML <script> tag, but
    that will change in the next iteration, as we move individual part of
    the application into modules

--- src/main.js
+++ src/main.js
@@ -1,38 +1,48 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow } = require("electron/main");
+const fs = require("fs");
+const path = require("path");
 
 // app, which controls your application's event lifecycle.
 // BrowserWindow, which creates and manages app windows.
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    })
-    win.loadFile('index.html')
-}
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+  win.loadFile("index.html");
+};
 
 // Many of Electron's core modules are Node.js event emitters that adhere to
 // Node's asynchronous event-driven architecture. The app module is one of
 // these emitters.
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'Darwin') {
-        app.quit()
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
     }
-})
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "Darwin") {
+    app.quit();
+  }
+});
 
 // You might have noticed the capitalization difference between the app and
 // BrowserWindow modules. Electron follows typical JavaScript conventions here,
 // where PascalCase modules are instantiable class constructors (e.g.
 // BrowserWindow, Tray, Notification) whereas camelCase modules are not
 // instantiable (e.g. app, ipcRenderer, webContents).
+
+// We want to bundle marked.js to we do not need to do any web request in index.html.
+function copyMarkedToRenderer() {
+  const source = path.join(__dirname, "node_modules/marked/marked.min.js");
+  const destination = path.join(__dirname, "renderer/assets/marked.min.js");
+
+  fs.copyFileSync(source, destination);
+}
