1f4248b9bb0427f9a3d3c160df30acdaed76792f
Martin Czygan <martin.czygan@gmail.com>
2025-01-20 14:07:38 +0100

    feat: editor modularization
    the editor now allows to run code after the initial "welcome" typing
    sequence stops; for that, the editor now uses promises to wait for the
    init code to finish.
    
    Also, there is a dummy message ("window.api.hey") in preload that makes
    it into the HTML view console.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      sandbox: false,
+    },
   });
   win.loadFile("index.html");
 };
