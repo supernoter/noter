5833cc8a72e3a910713fd258dd824242e7d27581
Martin Czygan <martin.czygan@gmail.com>
2025-02-16 13:51:21 +0100

    chore: remove wip snippets
    integrated, mainly in menu.js

--- src/wip/preload.js
+++
@@ -1,9 +0,0 @@
-// const { contextBridge, ipcRenderer } = require("electron");
-
-// const fs = require("fs");
-// const path = require("path");
-// const os = require("os");
-
-// contextBridge.exposeInMainWorld("api", {
-//   hey: "greetings from preload.js", // just a dummy string
-// });
--- src/wip/trigger.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require("electron");
-
-class Trigger {
-  constructor() {
-    this.textarea = document.getElementById("note-textarea");
-  }
-}
-
-const trigger = new Trigger();
