077345a95d88ba8a816de3036c92bcba08aab824
Martin Czygan <martin.czygan@gmail.com>
2025-02-16 12:20:49 +0100

    integration: intermediate step, load/save note
    load/save/menu functionality is carried on in wip/ and will be
    integrated in a second step into the current main branch

--- src/main.js
+++ src/main.js
@@ -1,40 +1,56 @@
-const { app, BrowserWindow, ipcRenderer } = require("electron");
-const menuitem = require("./customizations/menuItem.js");
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const windowHandler = require('./WindowHandler')
+const process = require('process')
+const fs = require('fs')
+const path = require('path')
 
-let window;
-const createWindow = () => {
-  window = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      contextIsolation: true,
-      enableRemoteModule: false,
-      sandbox: false,
-    },
-  });
-  menuitem.createMenu(window);
-  window.loadFile("index.html");
-};
+// Create path for all the user notes
+const notesDir = path.join(app.getPath('userData'), 'user-notes')
+
+ipcMain.handle('get-notes', async () => {
+    if (!fs.existsSync(notesDir)) {
+        fs.mkdirSync(notesDir, { recursive: true })
+
+        // Create an example markdown file
+        const defaultNotePath1 = path.join(notesDir, 'First note.md')
+        const defaultNotePath2 = path.join(notesDir, 'Second note.md')
+        const exampleNote1 = 'Hello, this is your first note!'
+        const exampleNote2 = 'This is your second note!'
+
+        fs.writeFileSync(defaultNotePath1, exampleNote1, 'utf-8')
+        fs.writeFileSync(defaultNotePath2, exampleNote2, 'utf-8')
+    }
+    return fs.readdirSync(notesDir)
+})
+
+ipcMain.handle('read-note', async (event, filename) => {
+    const filePath = path.join(notesDir, filename)
+    if (fs.existsSync(filePath)) {
+        return fs.readFileSync(filePath, 'utf8') // Return note content
+    }
+    return ''
+})
+
+// Many of Electron's core modules are Node.js event emitters that adhere to
+// Node's asynchronous event-driven architecture. The app module is one of
+// these emitters.
 
 app.whenReady().then(() => {
-  createWindow();
+    let window = windowHandler.createWindow()
+    window.loadFile('./index.html')
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            window = windowHandler.createWindow()
+            window.loadFile('./index.html')
+        }
+    })
+})
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+app.on('window-all-closed', () => {
+    if (process.platform !== 'Darwin') {
+        app.quit()
     }
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "Darwin") {
-    app.quit();
-  }
-});
+})
 
 // You might have noticed the capitalization difference between the app and
 // BrowserWindow modules. Electron follows typical JavaScript conventions here,
--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,12 @@
-// const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron')
+const customizationHandler = require('./CustomizationHandler')
 
-// const fs = require("fs");
-// const path = require("path");
-// const os = require("os");
+contextBridge.exposeInMainWorld('api', {
+    hey: 'greetings from preload.js', // just a dummy string
+    OLLAMA_HOST: process.env.OLLAMA_HOST || 'http://localhost:11434',
+    NOTER_OLLAMA_MODEL: process.env.NOTER_OLLAMA_MODEL || 'gemma',
+    getNotes: () => ipcRenderer.invoke('get-notes'),
+    readNote: (filename) => ipcRenderer.invoke('read-note', filename),
+})
 
-// contextBridge.exposeInMainWorld("api", {
-//   hey: "greetings from preload.js", // just a dummy string
-// });
+customizationHandler.applyCustomizationsToEditor()
