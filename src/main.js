56792346cf928e027d775881a051af666ac996a7
Martin Czygan <martin.czygan@gmail.com>
2025-02-27 11:41:59 +0100

    feat: make config file location platform independent
    This change allows us to use the "app" object in the
    configuration/customization module and to use platform specific path as
    documented here:
    
    https://www.electronjs.org/docs/latest/api/app#appgetpathname
    
    This required a couple of changes in main, preload as well, mostly to
    accomodate the "app" object passed into the various classes.
    
    The problem was that modules required by the initial module for electron
    do not have the "app" object created properly just yet. We have to move
    the initialization to a later stage.

--- src/main.js
+++ src/main.js
@@ -1,63 +1,74 @@
 const { app, BrowserWindow, ipcMain } = require('electron/main')
-const windowHandler = require('./WindowHandler')
-const menu = require('./menu')
 const process = require('process')
 const fs = require('fs')
 const path = require('path')
+const menu = require('./menu')
+const createWindowHandler = require('./WindowHandler')
+const createConfigurationInterface = require('./ConfigurationInterface')
 
 // Parse command line arguments
 const argv = process.argv.slice(1) // Remove the first element (path to electron executable)
 const showIntro = !argv.includes('--no-intro')
 
+// Reference to main window
+let mainWindow
+// Reference to window handler
+let windowHandler
+// Reference to configuration interface
+let configInterface
+
 // Create path for all the user notes
 const notesDir = path.join(app.getPath('documents'), 'noter')
-
 const WELCOME_NOTE_TEXT = `# Welcome to NOTER!
-
 Noter is a simple, yet powerful markdown editor that we hope you will enjoy using.
-
 Find out more at [supernoter.xyz](https://supernoter.xyz) and on GitHub at
 [supernoter/noter](https://github.com/supernoter/noter).
 `
 
-let mainWindow // reference to main window
+// Set up IPC handlers
+function setupIpcHandlers() {
+    ipcMain.handle('get-notes', async () => {
+        if (!fs.existsSync(notesDir)) {
+            fs.mkdirSync(notesDir, { recursive: true })
+            // create a welcome note
+            const welcomeNote = path.join(notesDir, 'Welcome.md')
+            fs.writeFileSync(welcomeNote, WELCOME_NOTE_TEXT, 'utf-8')
+        }
+        const files = fs
+            .readdirSync(notesDir)
+            .map((filename) => ({
+                name: filename,
+                path: path.join(notesDir, filename),
+                mtime: fs.statSync(path.join(notesDir, filename)).mtime,
+            }))
+            .sort((a, b) => b.mtime - a.mtime)
+            .map((file) => file.name)
+        return files
+    })
 
-ipcMain.handle('get-notes', async () => {
-    if (!fs.existsSync(notesDir)) {
-        fs.mkdirSync(notesDir, { recursive: true })
-        // create a welcome note
-        const welcomeNote = path.join(notesDir, 'Welcome.md')
-        fs.writeFileSync(welcomeNote, WELCOME_NOTE_TEXT, 'utf-8')
-    }
-    const files = fs
-        .readdirSync(notesDir)
-        .map((filename) => ({
-            name: filename,
-            path: path.join(notesDir, filename),
-            mtime: fs.statSync(path.join(notesDir, filename)).mtime,
-        }))
-        .sort((a, b) => b.mtime - a.mtime)
-        .map((file) => file.name)
-    return files
-})
+    ipcMain.handle('read-note', async (event, filename) => {
+        const filePath = path.join(notesDir, filename)
+        if (fs.existsSync(filePath)) {
+            mainWindow.webContents.send('set-editor-filepath', filePath)
+            return fs.readFileSync(filePath, 'utf8') // Return note content
+        }
+        return ''
+    })
 
-ipcMain.handle('read-note', async (event, filename) => {
-    const filePath = path.join(notesDir, filename)
-    if (fs.existsSync(filePath)) {
-        mainWindow.webContents.send('set-editor-filepath', filePath)
-        return fs.readFileSync(filePath, 'utf8') // Return note content
-    }
-    return ''
-})
+    // Add a new IPC handler to check if intro should be shown
+    ipcMain.handle('should-show-intro', () => {
+        return showIntro
+    })
 
-// Add a new IPC handler to check if intro should be shown
-ipcMain.handle('should-show-intro', () => {
-    return showIntro
-})
+    // Add handler to get configuration
+    ipcMain.handle('get-config', () => {
+        return configInterface.getConfigurationData()
+    })
+}
 
 // createInitialWindow creates a window, set the menu and loads our application
 // HTML.
-createInitialWindow = () => {
+function createInitialWindow() {
     mainWindow = windowHandler.createWindow()
     menu.createMenu(mainWindow)
     mainWindow.loadFile('./index.html')
@@ -67,12 +78,20 @@ createInitialWindow = () => {
     }
 }
 
-// Many of Electron's core modules are Node.js event emitters that adhere to
-// Node's asynchronous event-driven architecture. The app module is one of
-// these emitters.
-
+// Initialize the application
 app.whenReady().then(() => {
+    // Initialize configuration interface with app object
+    configInterface = createConfigurationInterface(app)
+
+    // Initialize window handler with app object
+    windowHandler = createWindowHandler(app)
+
+    // Set up IPC handlers
+    setupIpcHandlers()
+
+    // Create the main window
     createInitialWindow()
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createInitialWindow()
@@ -81,13 +100,7 @@ app.whenReady().then(() => {
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'Darwin') {
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
-
-// You might have noticed the capitalization difference between the app and
-// BrowserWindow modules. Electron follows typical JavaScript conventions here,
-// where PascalCase modules are instantiable class constructors (e.g.
-// BrowserWindow, Tray, Notification) whereas camelCase modules are not
-// instantiable (e.g. app, ipcRenderer, webContents).
