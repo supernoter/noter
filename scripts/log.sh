b4921c0b3143481bccb467fd0a0c79c6c9254fa5
Martin Czygan <martin.czygan@gmail.com>
2025-03-09 14:12:36 +0100

    add vc rendering

---
+++ scripts/log.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+OUTPUT=${1:-"VERSION-CONTROL-LOGS.pdf"}
+T=$(mktemp)
+
+S=$(mktemp).png
+
+termshot -f "$S" -c -- git cal
+
+echo "# VERSION CONTROL LOG ($(git rev-parse --short HEAD) - $(date))" >> $T
+
+cat >> "$T" <<'EOM'
+
+> UOL CM2020 2024/25 FINAL REPORT
+
+Generated with
+
+
+```
+$ date
+$ git summary
+$ git log --stat
+```
+
+Activity rendering generated with [git cal](https://github.com/k4rthik/git-cal).
+
+\tiny
+
+EOM
+
+echo "![Activity plot, generated with [git-cal](https://github.com/k4rthik/git-cal).]($S)" >> "$T"
+echo >> "$T"
+
+
+echo '----' >> "$T"
+echo '```' >> "$T"
+date >> "$T"
+echo >> "$T"
+
+
+git summary >> "$T"
+echo >> "$T"
+
+git log --stat >> "$T"
+echo '```' >> "$T"
+
+pandoc --template eisvogel -f markdown -t latex -o "$OUTPUT" "$T"
+rm -f "$T"
+
